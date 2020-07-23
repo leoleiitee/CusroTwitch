fff37988a2f9e823a83fc3ab2d482d9ff0f94f8e
Leonardo <leonardo.ribeiro@applysystem.com.br>
2020-07-22 21:35:54 -0300

    post inicial

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const root = document.querySelector('#root')
+const textTitle = 'Dificuldade'
+
+const title = Title`
+    color: #82589F; 
+    font-size: 2.5rem;
+    letter-spacing: 1.2px;
+    margin-bottom: 4rem;
+    ${textTitle}
+`
+
+root.insertAdjacentHTML('beforeend', title)
+root.insertAdjacentHTML('beforeend', wrapperCharacters)
